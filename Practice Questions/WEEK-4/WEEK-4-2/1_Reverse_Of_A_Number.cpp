70f795a042d74c8c9c72f7239df85e7ec99688be
Garima <saxenagarima2953@gmail.com>
2023-11-07 10:37:46 +0530

    Update 1_Reverse_Of_A_Number.cpp

---
+++ Practice Questions/WEEK-4/WEEK-4-2/1_Reverse_Of_A_Number.cpp
@@ -0,0 +1,19 @@
+//PROBLEM LINK -- https://www.codingninjas.com/studio/problems/reverse-of-a-number_624652
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n, rev=0, rem =0;
+	cin >> n;
+
+	while(n > 0){
+	rem = n%10;
+	rev = rev*10 + rem;
+	n = n/10;
+    }
+
+    cout << rev <<endl;
+
+    return 0;
+}
